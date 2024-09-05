5a6904d94860c303ac53d007f1f6b6479a18c4c3
23DP2ERemi <166109565+23DP2ERemi@users.noreply.github.com>
2024-09-05 10:21:53 +0000

    1. uzdevms

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -5,16 +5,33 @@ public class App
 {
     public static void main( String[] args )
     {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("kā tevi sauc?");
-        String name = scanner.nextLine();
-        System.out.println("Ievadi savu uzvardi:");
-        String surname = scanner.nextLine();
-        System.out.println("kurā grupa tu esi?");
-        String group = scanner.nextLine();
+        // Scanner scanner = new Scanner(System.in);
+        // System.out.println("kā tevi sauc?");
+        // String name = scanner.nextLine();
+        // System.out.println("Ievadi savu uzvardi:");
+        // String surname = scanner.nextLine();
+        // System.out.println("kurā grupa tu esi?");
+        // String group = scanner.nextLine();
 
-        System.out.println("tu atbildeji: " + name + " " + surname +  " " + group);
+        // System.out.println("tu atbildeji: " + name + " " + surname +  " " + group);
 
+        int Chicken = 9000;
+        double Bacon = 0.1;
+        String Tractor = "Zetor";
+
+        System.out.println("Chicken:");
+        System.out.println(Chicken);
+        System.out.println("Bacon (kg):");
+        System.out.println(Bacon);
+        System.out.println("Tractor:");
+        System.out.println(Tractor);
+
+        System.out.println(" ");
+
+        System.out.println("And finally, a sumary:");
+        System.out.println(Chicken);
+        System.out.println(Bacon);
+        System.out.println(Tractor);
 
 
     }
