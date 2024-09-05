020024190b937a5db0e902b708ed41edc32fd73f
23DP2ERemi <166109565+23DP2ERemi@users.noreply.github.com>
2024-09-05 09:50:27 +0000

    uzdevums 0

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -1,10 +1,25 @@
 package lv.rvt;
+import java.util.Scanner;
 
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println("Hello world!");        
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("kā tevi sauc?");
+        String name = scanner.nextLine();
+        System.out.println("Ievadi savu uzvardi:");
+        String surname = scanner.nextLine();
+        System.out.println("kurā grupa tu esi?");
+        String group = scanner.nextLine();
+
+        System.out.println("tu atbildeji:");
+        System.out.println(name);
+        System.out.println(surname);
+        System.out.println(group);
+
+
     }
 
+   
 }
