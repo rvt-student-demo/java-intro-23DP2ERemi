60f79c1bf0d3cf9c66b3fc6e0c0dc848c71a01c6
23DP2ERemi <166109565+23DP2ERemi@users.noreply.github.com>
2024-10-08 07:35:43 +0000

    Star sign (4 parts)

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -3,27 +3,43 @@ import java.util.Scanner;
 
 public class App 
 {
-    // public static void main(String[] args) {
-    //     printUntilNumber(5);
-    // }
-    // public static void printUntilNumber(int num){
-    //     for (int i = 1; i < num + 1; i++) {
-    //         System.out.println(i);
-    //     }
-    // }
+        public static void main(String[] args) {
+        printStars(5);
+        printStars(3);
+        printStars(9);
+        System.out.println("");
+        printSquare(4);
+        System.out.println("");
+        printRectangle(17, 3);
+        System.out.println("");
+        printTriangle(5);
+        }
 
+        public static void printStars(int number) {
+            for (int i = 0; i < number; i++) {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
 
-    public static void main(String[] args) {
-        divisibleByThreeInRange(3, 100);
-    }
+        public static void printSquare(int number){
+            for (int i = 0; i < number; i++) {
+                printStars(number);
+            }
+            System.out.println();
+        }
 
-    public static void divisibleByThreeInRange(int start, int end){
-        for (int i = start; i < end + 1; i++) {
-            if (i % 3 == 0){
-                System.out.println(i);
+        public static void printRectangle(int width, int height){
+            for (int i = 0; i < height; i++) {
+                printStars(width);
             }
         }
-    }
+        
+        public static void printTriangle(int number){
+            for (int i = 0; i < number; i++) {
+                printStars(i);
+            }
+        }
 }
 
 
