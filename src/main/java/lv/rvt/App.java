e37061d36d25c56e6d78d50c6d00479aeeec55d0
23DP2ERemi <166109565+23DP2ERemi@users.noreply.github.com>
2024-10-08 09:40:28 +0000

    advanced astrology

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -1,5 +1,4 @@
 package lv.rvt;
-import java.util.Scanner;
 
 public class App 
 {
@@ -13,6 +12,9 @@ public class App
         printRectangle(17, 3);
         System.out.println("");
         printTriangle(5);
+        System.out.println("");
+        printTriangleTwo(3);
+        System.out.println(4);
         }
 
         public static void printStars(int number) {
@@ -40,6 +42,43 @@ public class App
                 printStars(i);
             }
         }
+
+        // advanced astrology:
+
+        public static void printSpaces(int number){
+            for (int i = 0; i < number; i++) {
+                System.out.print(" ");
+             }
+        }
+
+        public static void printTriangleTwo(int size){
+            int j = 1;
+            for (int i = size; j > 0; i--) {
+                printSpaces(i);
+                if(j < size+1){
+                    printStars(j);
+                    j++;
+                }
+            }
+        }
+
+        public static void printChristmasTree(int height){
+            for (int i = 1; i < height*height; i+=2) {
+                printStars(i);
+            }
+            int j = 1;
+            for (int i = height; i > 0; i--) {
+                printSpaces(i);
+                if(j < height*height){
+                    printStars(j);
+                    j+=2;
+                }
+            }
+            for (int i = 0; i < 2; i++) {
+                printSpaces(j/2-1);
+                printStars(3);
+            }
+            }
 }
 
 
