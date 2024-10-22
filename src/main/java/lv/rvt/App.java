d5f70770959d38288fb5cb1c27474cc16cd43037
23DP2ERemi <166109565+23DP2ERemi@users.noreply.github.com>
2024-10-22 09:33:33 +0000

    same sum

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -5,158 +5,120 @@ import java.util.Scanner;
 public class App 
 {
 
+public static void main(String[] args) {
+    int[] valA = {13,-22,82, 17};
+    int[] valB = {0, 0, 0, 0};
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        ArrayList<Integer> nums = new ArrayList<>();
-        System.out.println("Give number ");
-        while (true) {
-            int num = scanner.nextInt();
-            if (num == 0) {
-                break;
-        }
-        nums.add(num);
-
-        
+    for (int i = 0; i < valB.length; i++) {
+        valB[i] = 25 -  valA[i];
     }
-    System.out.println("From where: ");
-        int start = scanner.nextInt();
 
-        System.out.println("To where: ");
-        int end = scanner.nextInt();
+    System.out.println("ValA: "
+    + valA[0] + " " + valA[1] + " "  + valA[2] + " " + valA[3]);
 
-        for (int i = start; i <= end; i++) {
-            System.out.println(nums.get(i));
-        }
-        // System.out.println("Write a number: ");
-        // num = 0;
-        // while(num != -1){
-        //     Integer num = scanner.nextLine();
-        //     nums.add(num);
-        // }
-        // System.out.println(nums);
-    }
-        
-    // public static void main(String[] args) {
-    //     ArrayList<String> friends = new ArrayList<>();
-
-    //     System.out.println("Write a name: ");
-    //     Scanner scanner = new Scanner(System.in);
-    //     String name = scanner.nextLine();
-    //    while(name != null || name != "") {
-
-    //     friends.add(name);
-    //     name = scanner.nextLine();
-    //     // if(name.equals("")){
-            
-    //     // }
-    //    }
-        
-
-        // System.out.println(friends.get(0));
-
-            // public static void main(String[] args){
-            //     ArrayList<Integer> ints = new ArrayList<>();
-
-            //     Scanner scanner = new scanner(System.on);
-            //     while(True){
-            //         int input = Integer.valueOf(scanner.nextLine());
-            //         if (input == 0){
-            //             break;
-            //         }
-            //         if (input > 0 || input < 0){
-            //             ints.add(input);
-            //         }
-            //     }
-            //     int sum = 0;
-            //     for (int i = 0; i < ints; i++) {
-                    
-            //     }
-
-            // }
-
-
-
-
-
-
-
-    }
-        // public static void printStars(int number) {
-        //     for (int i = 0; i < number; i++) {
-        //         System.out.print("*");
-        //     }
-        //     System.out.println("");
-        // }
-
-        // public static void printSquare(int number){
-        //     for (int i = 0; i < number; i++) {
-        //         printStars(number);
-        //     }
-        //     System.out.println();
-        // }
-
-        // public static void printRectangle(int width, int height){
-        //     for (int i = 0; i < height; i++) {
-        //         printStars(width);
-        //     }
-        // }
-        
-        // public static void printTriangle(int number){
-        //     for (int i = 0; i < number; i++) {  m.l 
-        //         printStars(i);
-        //     }
-        // }
-
-        // // advanced astrology:
-
-        // public static void printSpaces(int number){
-        //     for (int i = 0; i < number; i++) {
-        //         System.out.print(" ");
-        //      }
-        // }
-
-        // public static void printTriangleTwo(int size){
-        //     int j = 1;
-        //     for (int i = size; j > 0; i--) {
-        //         printSpaces(i);
-        //         if(j < size+1){
-        //             printStars(j);
-        //             j++;
-        //         }
-        //     }
-        // }
-
-        // public static void printChristmasTree(int height){
-        //     int j = 1;
-        //     for (int i = height; i > 0; i--) {
-        //         printSpaces(i);
-        //         if (j < height * height) {
-        //             printStars(j);
-        //             j+=2;
-        //         }
-        //     }
-        //     for (int i = 0; i < 2; i++) {
-        //         printSpaces(j/2-1);
-        //         printStars(3);
-        //     }
-        // }
-
-        // public static void main(String[] args) {
-        // // printStars(5);
-        // // printStars(3);
-        // // printStars(9);
-        // // System.out.println("");
-        // // printSquare(4);
-        // // System.out.println("");
-        // // printRectangle(17, 3);
-        // // System.out.println("");
-        // // printTriangle(5);
-        // // System.out.println("");
-        // // printTriangleTwo(3);
-        // // System.out.println(4);
-        //    printChristmasTree(50);
-        //  }
-
+    System.out.println("ValB: "
+    + valB[0] + " " + valB[1] + " "  + valB[2] + " " + valB[3]);
 
     
+    System.out.println("sum: "
+    + (valA[0]+valB[0]) + " " + (valA[1]+valB[1] + " " + (valA[2]+valB[2]) + " " + (valA[3]+valB[3])));
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//     public static void main(){
+//         printNumbersInRange(null, i, i);
+//     }
+//     public static void printNumbersInRange(ArrayList<Integer> numbers, int lowerLimit, int upperLimit){
+//         ArrayList<Integer> numbers = new ArrayList<>();
+//         numbers.add(3);
+//         numbers.add(2);
+//         numbers.add(6);
+//         numbers.add(-1);
+//         numbers.add(5);
+//         numbers.add(1);
+
+//         System.out.println("The numbers in the range [0, 5]");
+//         printNumbersInRange(numbers, 0, 5);
+
+//         System.out.println("The numbers in the range [3, 10]");
+//         printNumbersInRange(numbers, 3, 10);
+//     }
+
+// for (int i : numbers){
+//     if (i >= lowerLimit && i <= upperLimit){
+//         System.out.println(i);
+//     }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // public static void main(String[] args) {
+    //     Scanner scanner = new Scanner(System.in);
+    //     ArrayList<Integer> nums = new ArrayList<>();
+    //     System.out.println("Give number ");
+    //     while (true) {
+    //         int num = scanner.nextInt();
+    //         if (num == -1) {
+    //             break;
+    //     }
+    //     nums.add(num);
+
+
+        
+        
+    // }
+    // System.out.println("From where: ");
+    //     int start = scanner.nextInt();
+
+    //     System.out.println("To where: ");
+    //     int end = scanner.nextInt();
+
+    //     for (int i = start; i <= end; i++) {
+    //         System.out.println(nums.get(i));
+    //     }
