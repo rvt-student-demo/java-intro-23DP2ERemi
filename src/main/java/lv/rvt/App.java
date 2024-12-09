3fb398670f25f5cddc082e8f7ea83955b28ba3a1
23DP2ERemi <A231498ER@rvt.lv>
2024-12-09 22:50:41 +0200

    komitoju lai parbaudit vai codespace strada no citas ierices

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -8,23 +8,23 @@ public class App {
 
     public static void main(String[] args) throws IOException, Exception { 
 
-        Scanner scanner = new Scanner(System.in);
+        // Scanner scanner = new Scanner(System.in);
 
-        System.out.println("name:");
-        String name = scanner.nextLine();
-        System.out.println("age:");
-        int age = scanner.nextInt();
-        scanner.nextLine(); 
+        // System.out.println("name:");
+        // String name = scanner.nextLine();
+        // System.out.println("age:");
+        // int age = scanner.nextInt();
+        // scanner.nextLine(); 
 
-        Person person = new Person(name, age);
-        PersonManager.addPerson(person);
-        System.out.println("Thank you for registering");
+        // Person person = new Person(name, age);
+        // PersonManager.addPerson(person);
+        // System.out.println("Thank you for registering");
 
-        ArrayList<Person> persons = PersonManager.getPersonList(); 
-        for (Person p : persons) {
-            System.out.println(p);
-        }
-
-        scanner.close();
+        // ArrayList<Person> persons = PersonManager.getPersonList(); 
+        // for (Person p : persons) {
+        //     System.out.println(p);
+        // }
+   
+        // scanner.close();
     }
 }
