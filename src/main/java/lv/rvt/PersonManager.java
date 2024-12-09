e0b09bdbc85361d73fb2ac4dfd6298473fe673de
23DP2ERemi <A231498ER@rvt.lv>
2024-12-10 00:03:22 +0200

    uzlaboju personManager(part1) un personManager(part2)

---
+++ src/main/java/lv/rvt/PersonManager.java
@@ -0,0 +1,34 @@
+package lv.rvt;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.nio.file.StandardOpenOption;
+import java.util.ArrayList;
+
+public class PersonManager {
+    public static ArrayList<Person> getPersonList() throws Exception {
+
+        ArrayList<Person> persons = new ArrayList<>();
+        BufferedReader reader = Helper.getReader("src/data/person.csv");
+
+        reader.readLine();
+        String line;
+
+
+        while ((line = reader.readLine()) != null) {
+            String[] data = line.split(", ");
+
+            Person person1 = new Person(data[0], Integer.valueOf(data[1]), Double.valueOf(data[2]), Double.valueOf(data[3]));
+            persons.add(person1);
+        }
+        reader.close();
+        return persons;
+    }
+
+    public static void addPerson(Person person) throws Exception {
+        BufferedWriter writer = Helper.getWriter("person.csv", StandardOpenOption.APPEND);
+        writer.write(person.toCsvRow());
+        writer.newLine();
+        writer.close();
+    }
+}
